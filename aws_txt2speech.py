3242420273e26f8db10cdaebe6b439ec46799e15
Kraftbar <gautenybo@gmail.com>
2021-09-02 16:14:09 +0200

    update

--- aws_txt2speech.py
+++ aws_txt2speech.py
@@ -3,16 +3,15 @@ import time
 import sys
 
 
-
-
 print("Hello World!")
+
+
 try:
     f = open("input.txt", "r")
     lines= f.readlines()
     txt="".join(lines)
 except :
     if not sys.stdin.isatty():
-        print("reading from stdin")
         print("reading from stdin")
         txt = sys.stdin.read()
     else:
@@ -21,19 +20,20 @@ except :
         sys.exit()
 
 
-
-
 ##
 # make sure to config "~/.aws/config" and "~/.aws/credentials"
 
 #polly_client = boto3.Session(aws_access_key_id='',aws_secret_access_key='', region_name='eu-north-1').client('polly')
 polly_client = boto3.Session().client('polly')
+
 time.sleep(0.8)
 response = polly_client.start_speech_synthesis_task(VoiceId='Brian',
                 OutputS3BucketName='mytest9752635',
                 OutputS3KeyPrefix='5a4365a713a9a28144cae362e50fceed47dab84db405a5c7d4a6b216176ac50d',
-                OutputFormat='mp3', 
+                OutputFormat='pcm', 
                 Text = txt)
+
+
 
 OutputUri = response['SynthesisTask']['OutputUri']
 taskId =    response['SynthesisTask']['TaskId']
@@ -52,15 +52,14 @@ while(current_task_status != 'completed'):
 #s3 =  boto3.client('s3',aws_access_key_id='',aws_secret_access_key='')
 s3 = boto3.client('s3')
 print("Downloading...")
-s3.download_file('mytest9752635',objectName,'out.mp3')
+s3.download_file('mytest9752635',objectName,'out.wav')
 time.sleep(2)
 print("Deleting s3 object...")
 s3.delete_object(Bucket='mytest9752635',Key=objectName)
 
+os.system("aplay -r 16000 out.wav ") 
+os.system("rm out.wav ") 
 
     
     
     
-    
-    
-
